f806bbe615aa9fcbd5a3374223a258e8a103b901
Subrahmanyam Singam <66869607+subbu2k@users.noreply.github.com>
2020-10-26 17:51:47 +0530

    extended read and write functions

--- simple_char_device.c
+++ simple_char_device.c
@@ -1,7 +1,8 @@
 #include<linux/init.h>
 #include<linux/module.h>
 #include<linux/fs.h>
-
+int l;
+static char msg[10];
 int open_file(struct inode *pinode, struct file *pfile)
 {
 	printk(KERN_ALERT "Inside the %s function\n",__FUNCTION__);
@@ -11,12 +12,27 @@ int open_file(struct inode *pinode, struct file *pfile)
 ssize_t read_file(struct file *pfile,char __user *buffer,size_t length,loff_t *offset)
 {
 	printk(KERN_ALERT "Inside the %s function\n",__FUNCTION__);
+	l=0;
+	//printk("%s",msg);
+	while(l<length)
+	{
+		put_user(msg[l],buffer+l);
+		l++;
+	}
         return 0;
 }
 
 ssize_t write_file(struct file *pfile,const char __user *buffer,size_t length,loff_t *offset)
 {
 	printk(KERN_ALERT "Inside the %s function\n",__FUNCTION__);
+	copy_from_user(msg,buffer,length);
+	l=0;
+	while(l<length)
+	{
+		msg[l]+=3;
+		l++;
+	}
+	//printk("%s",msg);
         return length;
 }
 
--- userprog.c
+++ userprog.c
@@ -5,13 +5,17 @@
 
 int main ()
 {
-	char buffer [1];
+	char buffer[3];
+	char ibuffer[3];
 	int fd;
 	fd=open ("/dev/simple_char_device",O_RDWR);
-	printf("%d",fd);
-	buffer[0]=1;
-	write(fd,buffer,1);
-	read(fd,buffer,1);
-	printf("Value : 0x %02x\n",buffer[0]);
+	//printf("%d",fd);
+	buffer[0]='a';
+	buffer[1]='b';
+	buffer[2]='c';
+	write(fd,buffer,3);
+	read(fd,ibuffer,3);
+	printf("%s",ibuffer);
+	//printf("Value : 0x %02x\n",buffer[0]);
 	close(fd);
 }
