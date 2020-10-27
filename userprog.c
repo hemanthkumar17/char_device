db63a97a3c04937087f89cdca3cdd5739b1b678f
hemanthkumar17 <hemanth17112000@gmail.com>
2020-10-27 19:14:41 +0530

    FIX: User program commented

--- userprog.c
+++ userprog.c
@@ -1,24 +1,42 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 
-int main ()
-{
-	char buffer[10];
-	int n,fd;
-	printf("Enter message length: ");
-	scanf("%d",&n);
-	fd=open ("/dev/enc_device",O_RDWR);
+int main(){
+	// To store the message to be encrypted and decrypted
+	char buffer[100];
+	//fd -> File Descriptor    n-> Length of the message
+	int fd,n=0;
+	
 	printf("Enter message : ");
 	scanf("%s",buffer);
+	while(buffer[n++] != '\0');
+	n--;
+	
+	fd=open("/dev/enc_device",O_RDWR);
+	if(fd<0){
+		printf("Cannot open Encryption char device file\n");
+		return 0;
+	}
+	//Write to device
 	write(fd,buffer,n);
+	// Read from device to user
 	read(fd,buffer,n);
 	printf("The encoded message is \"%s\"\n",buffer);
 	close(fd);
+	
+	
 	fd=open ("/dev/dec_device",O_RDWR);
+	if(fd<0){
+		printf("Cannot open Decryption char device file\n");
+		return 0;
+	}
+	//Write to device
         write(fd,buffer,n);
+        
+        //Read from device to user
         read(fd,buffer,n);
+        
         printf("The decoded message is \"%s\"\n",buffer);
         close(fd);
 }
